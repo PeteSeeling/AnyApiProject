caf9abb8539a0d32a57a7273f70e5570acc62d68
Pete Seeling <peteseeling@gmail.com>
2022-03-15 16:09:21 -0600

    test added. controller file made

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,23 @@ describe('anyapiproject1 routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it ('creates a book', async () => {
+    const expected = {
+      title:'War is a Racket',
+      author:'Smedly Butler',
+      pages:'120'
+      
+    };
+    const res = await (await request(app).post('api/v1/books')).setEncoding(expected);
+
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a list of books', async () => {
+    const expected = await Book.findAll();
+    const res = await request(app).get('/api/v1/books');
+
+    expect(res.body).toEqual(expected);
+  });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    pages INT
+    
+);
+
+INSERT INTO
+ books (title, author, pages)
+
+ VALUES
+ ('War is a Racket', 'Smedly Butler', '120')
+ ('Red Notice', 'Bill Browder', '228')
