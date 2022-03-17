81527a8450451ec1cd05f48bff1635cb1fb68469
Pete Seeling <peteseeling@gmail.com>
2022-03-16 18:57:09 -0600

    1 test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -26,7 +26,7 @@ describe('anyapiproject1 routes', () => {
   });
 
   it('gets a list of books', async () => {
-    const expected = await Book.findAll();
+    const expected = await Book.listAllRows();
     const res = await request(app).get('/api/v1/books');
 
     expect(res.body).toEqual(expected);
@@ -43,12 +43,12 @@ describe('anyapiproject1 routes', () => {
       id:expect.any(String),
       title:'War is a Racket',
       author: 'Smedly Butler',
-      pages:'120',
-      edition: '4th',
+      pages:'122',
+    
     };
     const res = await request(app)
       .patch('/api/v1/books/1')
-      .send({ edition:'4th' });
+      .send({ pages:'122' });
 
     expect(res.body).toEqual(expected);
   });
